39f0c4039468b6081a2235e9630db292904d2248
khadijagit <khadijatoug10@gmail.com>
2022-07-17 03:02:55 +0000

    oki

--- app/src/main/java/us/sambaai/etablissement/MainActivity.java
+++ app/src/main/java/us/sambaai/etablissement/MainActivity.java
@@ -1,8 +1,8 @@
 package us.sambaai.etablissement;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -10,22 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.util.HashMap;
 
-import us.sambaai.etablissement.connection.Connection;
 
 public class MainActivity extends AppCompatActivity {
     Button envoyer;
     EditText nom,prenom, classe;
-
-
+    ProgressDialog dialog;
+    JSONParser parser= new JSONParser();
+    int success;
+    //String url="http://localhost/etablissement/insert.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,73 +34,53 @@ public class MainActivity extends AppCompatActivity {
         envoyer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nom1 = nom.getText().toString();
-                String surname = prenom.getText().toString();
-                String classroom = classe.getText().toString();
-                new InsertData().execute(nom1,surname, classroom);
+                new Add().execute();
             }
         });
 
     }
-    class InsertData extends AsyncTask<String, Void, String>
+    class Add extends AsyncTask<String, String, String>
     {
-        @Override
-        protected String doInBackground(String... strings) {
-            try
-            {
-                String link = Connection.API + "insertion.php";
-
-                String data = URLEncoder.encode("nom", "UTF-8") + "=" + URLEncoder.encode (strings[0],  "UTF-8") ;
-                data += "&" + URLEncoder.encode( "prenom", "UTF-8") + "=" + URLEncoder.encode (strings[1],  "UTF-8");
-                data += "&" + URLEncoder.encode( "classe",  "UTF-8") + "=" + URLEncoder.encode (strings[2],  "UTF-8");
-
-                URL url = new URL(link);
-
-                URLConnection connection = url.openConnection();
-                connection.setDoOutput(true);
-
-                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-                writer.write(data);
-                writer.flush();
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                return reader.readLine();
-            }
-
-
-            catch (Exception e)
-            {
-                e.printStackTrace();
-                return "Error! " + e.getMessage();
-            }
-        }
 
         @Override
         protected void onPreExecute(){
             super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setMessage("Patientez SVP");
+            dialog.show();
         }
+        @Override
+        protected String doInBackground(String... strings) {
+            HashMap<String,String> map = new HashMap<String, String>();
+            map.put("nom",nom.getText().toString());
+            map.put("prenom",nom.getText().toString());
+            map.put("classe",nom.getText().toString());
+            JSONObject object= parser.makeHttpRequest("http://192.168.1.12/etablissement/add.php", "GET",map);
+
+            try {
+                success=object.getInt("success");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+
 
         @Override
         protected void onPostExecute(String s) {
-            try
+            super.onPostExecute(s);
+            dialog.cancel();
+
+            if(success==1)
             {
-                JSONObject jsonObject = new JSONObject(s);
-
-                if(jsonObject.getString( "response").equals("success"))
-                {
-                    Toast.makeText(MainActivity.this, "Enregistre avec success", Toast.LENGTH_LONG).show();
-                }
-		        else
-                {
-                    Toast.makeText(MainActivity.this, "Erreur", Toast.LENGTH_LONG).show();
-                }
-
+                Toast.makeText(MainActivity.this, "Ajout effectue", Toast.LENGTH_LONG).show();
             }
-
-            catch (Exception e)
+            else
             {
-                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Echec !!! ", Toast.LENGTH_LONG).show();
             }
         }
 
--- app/src/main/java/us/sambaai/etablissement/connection/Connection.java
+++
@@ -1,6 +0,0 @@
-package us.sambaai.etablissement.connection;
-
-public class Connection {
-    public static final String API = "192.168.1.12";
-
-}
